94b106b1e2c096eff053163d54ae908097b77163
Juan Roldán <juan.roldan.zamudio@gmail.com>
2024-12-20 11:01:29 -0600

    Agregue algunas notas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+// IMPORTs
+// Styles
 import "./Styles/index.css";
+
+// React
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+// Components
+import App from "./App.jsx";
 import Context from "./Context/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
